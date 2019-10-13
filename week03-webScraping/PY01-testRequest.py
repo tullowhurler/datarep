7b18e8c846a694fad31cac54c5449ada7164f73d
tullowhurler <G00364760@gmit.ie>
2019-10-13 20:18:34 +0100

    last one

--- week03-webScraping/PY01-testRequest.py
+++ week03-webScraping/PY01-testRequest.py
@@ -1,11 +1,7 @@
 import requests
 from bs4 import BeautifulSoup 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html") 
-print (page) 
-print("-------------------") 
-print (page.content) 
-soup1 = BeautifulSoup(page.content, 'html.parser') 
-print("-------------------") 
-print (soup1.prettify()) 
+with open("../week02-carview\carviewer.html") as fp:     
+    soup = BeautifulSoup(fp,'html.parser') 
+print (soup.prettify()) 
  
  
