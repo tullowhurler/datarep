14c54c6789a9eca6fc72a8fe73c6d66a867c2f07
tullowhurler <G00364760@gmit.ie>
2019-11-05 19:10:47 +0000

    fixed directory

--- week05/server/simpleserver.py
+++ week05/server/simpleserver.py
@@ -4,7 +4,7 @@ from flask import Flask
  
 @app.route('/') 
 def index():     
-return"Hello, World!" 
+    return"Hello, World!" 
  
 if __name__ == '__main__' :     
-app.run(debug= True)
+    app.run(debug= True)
