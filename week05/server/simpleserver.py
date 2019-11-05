d711d14ae9ecea803a7f38dc15705e3def2c65a4
tullowhurler <G00364760@gmit.ie>
2019-11-05 19:25:34 +0000

    index

--- week05/server/simpleserver.py
+++ week05/server/simpleserver.py
@@ -1,6 +1,10 @@
 #!flask/bin/python 
 
 from flask import Flask 
+
+app = Flask(__name__,             
+            static_url_path='', 
+            static_folder='../')
  
 @app.route('/') 
 def index():     
