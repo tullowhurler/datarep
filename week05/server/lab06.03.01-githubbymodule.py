a72e75002fd92bc1660737fd6f5b6ecf76d30585
tullowhurler <G00364760@gmit.ie>
2019-11-06 22:27:03 +0000

    trying to fix errors

--- week05/server/lab06.03.01-githubbymodule.py
+++ week05/server/lab06.03.01-githubbymodule.py
@@ -7,8 +7,8 @@ g = Github("b55d312da577ba479f7dc4f8f3f5b1384bdf3b2e")
 for repo in g.get_user().get_repos():     
     #print(repo.name) 
  
-repo = g.get_repo("datarepresentationstudent/aPrivateOne") 
- #print(repo.clone_url
+    repo = g.get_repo("datarepresentationstudent/aPrivateOne") 
+ #print(repo.clone_url)
 
 fileInfo = repo.get_contents("test.txt") 
 urlOfFile = fileInfo.download_url 
@@ -18,6 +18,9 @@ urlOfFile = fileInfo.download_url
 response = requests.get(urlOfFile)
 contentOfFile = response.text 
 #print (contentOfFile)
+
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",newContents ,fileInfo.sha) 
+print (gitHubResponse)
 
 newContents = contentOfFile + " more stuff \n" 
 print (newContents)
