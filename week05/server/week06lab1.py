fa93ab151452c3f053789ad4b7bfde4048feb2f9
tullowhurler <G00364760@gmit.ie>
2019-11-06 21:21:48 +0000

    last step

--- week05/server/week06lab1.py
+++ week05/server/week06lab1.py
@@ -1,7 +1,28 @@
 import requests 
 url = "http://127.0.0.1:5000/cars"
-
 import json 
+
+from xlwt import *
+response = requests.get(url) 
+data = response.json() 
+#other code 
+# # write to excel file 
+w = Workbook() 
+ws = w.add_sheet('cars') 
+row = 0 
+ws.write(row,0,"reg") 
+ws.write(row,1,"make") 
+ws.write(row,2,"model") 
+ws.write(row,3,"price") 
+row += 1  
+for car in data["cars"]:     
+    ws.write(row,0, car["reg"])       
+    ws.write(row,1,car["make"])       
+    ws.write(row,2,car["model"])       
+    ws.write(row,3,car["price"])     
+    row += 1 
+w.save('cars.xls')
+
  #other code 
  #save this to a file 
 filename = 'cars.json' 
@@ -10,10 +31,7 @@ if filename:
   with open(filename, 'w') as f:         
     json.dump(data, f, indent=4)
 
-#response = requests.get(url) 
-#data = response.json() 
- 
-#for car in data["cars"]:     
-    #print (car)
+for car in data["cars"]:     
+    print (car)
 #output to console 
-#print (data)
+print (data)
