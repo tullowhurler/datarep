277db6951706fffe87ae381eabf34b8a6dcccf78
tullowhurler <G00364760@gmit.ie>
2019-11-06 21:14:16 +0000

    working through lab

--- week05/server/week06lab1.py
+++ week05/server/week06lab1.py
@@ -1,8 +1,19 @@
 import requests 
- url = "http://127.0.0.1:5000/cars"
-  
- response = requests.get(url) 
- data = response.json() 
+url = "http://127.0.0.1:5000/cars"
+
+import json 
+ #other code 
+ #save this to a file 
+filename = 'cars.json' 
+if filename:     
+     # Writing JSON data     
+  with open(filename, 'w') as f:         
+    json.dump(data, f, indent=4)
+
+#response = requests.get(url) 
+#data = response.json() 
  
- #output to console 
- print (data)
+#for car in data["cars"]:     
+    #print (car)
+#output to console 
+#print (data)
