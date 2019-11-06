f5bdc2b406929126e88194e08b4b89799885dec4
tullowhurler <G00364760@gmit.ie>
2019-11-06 22:16:49 +0000

    downloaded gittyhub

---
+++ week05/server/lab06.03.01-githubbymodule.py
@@ -0,0 +1,23 @@
+from github import Github 
+ 
+# remove the minus sign from the key 
+
+g = Github("b55d312da577ba479f7dc4f8f3f5b1384bdf3b2e") 
+ 
+for repo in g.get_user().get_repos():     
+    #print(repo.name) 
+ 
+repo = g.get_repo("datarepresentationstudent/aPrivateOne") 
+ #print(repo.clone_url
+
+fileInfo = repo.get_contents("test.txt") 
+urlOfFile = fileInfo.download_url 
+ 
+ #print (urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text 
+#print (contentOfFile)
+
+newContents = contentOfFile + " more stuff \n" 
+print (newContents)
