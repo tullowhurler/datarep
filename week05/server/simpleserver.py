af911ba1afb5dbb3b3ec0989158ec732da9be7cd
tullowhurler <G00364760@gmit.ie>
2019-12-10 18:28:14 +0000

    updating the repository from work already done

--- week05/server/simpleserver.py
+++ week05/server/simpleserver.py
@@ -2,9 +2,7 @@
 
 from flask import Flask 
 
-app = Flask(__name__,             
-            static_url_path='', 
-            static_folder='../')
+app = Flask(__name__, static_url_path='', static_folder='../')
  
 @app.route('/') 
 def index():     
